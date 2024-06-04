027aafb5c6561ad993f4ed84d8d4ebc6a386375a
vikasmehar11 <vikasmehar11@gmail.com>
2024-06-04 17:42:53 +0530

    Your commit message here

--- comments/app/settings.py
+++ comments/app/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('django-insecure-cdx$0o6-(_ppx6u1t!0z()v=+%(njf$al04@k_v1h$!b0#)crj')
+# SECRET_KEY = config('django-insecure-cdx$0o6-(_ppx6u1t!0z()v=+%(njf$al04@k_v1h$!b0#)crj')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config(True)
